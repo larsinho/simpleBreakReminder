edbd47eaef8c990e2cdd3b12551e90641d60334d
larsinho <lars.sendstad@gmail.com>
2018-10-14 15:09:19 +0200

    simple example that alerts for a set interval to take a break

---
+++ background.js
@@ -0,0 +1,36 @@
+
+
+console.log("background running");
+
+
+
+
+chrome.alarms.create('arbitrary', {
+    when: 1000,
+    periodInMinutes: 1
+});
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    console.log(alarm)
+    let msg = {
+        txt: "break"
+    }
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, msg, function(response) {
+          console.log(response.farewell);
+        });
+      });
+});
+
+
+
+  
+chrome.browserAction.onClicked.addListener(buttonClicked);
+// Sends a message to content.js
+function buttonClicked(tab) {
+    console.log("button clicked!");
+    let msg = {
+        txt: "hello"
+    }
+    chrome.tabs.sendMessage(tab.id, msg);
+}
---
+++ content.js
@@ -0,0 +1,23 @@
+
+// chrome://extensions
+// enabel "devloper mode"
+
+console.log("running")
+let alarmResponse 
+
+
+chrome.runtime.onMessage.addListener(gotMessage)
+
+function gotMessage(message, sender, sendResponse) {
+    console.log(message.txt)
+    if (message.txt === "break") {
+        alert("break")
+    }
+}
+
+// Function to put up window
+
+//let paragraphs = document.getElementsByTagName("p");
+// for (var elt of paragraphs) {
+//     elt.style['background-color'] = '#551a8b'
+// }
